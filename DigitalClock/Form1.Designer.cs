11b9d60a4223719bfb22d72482a920661929874b
Alexander Krakoczki <alexander.krakoczki@gmail.com>
2022-11-07 21:36:24 +0100

    First project on github

---
+++ DigitalClock/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace DigitalClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timeLbl = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dateLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timeLbl
+            // 
+            this.timeLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.timeLbl.ForeColor = System.Drawing.Color.White;
+            this.timeLbl.Location = new System.Drawing.Point(212, 106);
+            this.timeLbl.Name = "timeLbl";
+            this.timeLbl.Size = new System.Drawing.Size(461, 131);
+            this.timeLbl.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dateLbl
+            // 
+            this.dateLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dateLbl.ForeColor = System.Drawing.Color.White;
+            this.dateLbl.Location = new System.Drawing.Point(212, 247);
+            this.dateLbl.Name = "dateLbl";
+            this.dateLbl.Size = new System.Drawing.Size(521, 131);
+            this.dateLbl.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateLbl);
+            this.Controls.Add(this.timeLbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timeLbl;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label dateLbl;
+    }
+}
+
---
+++ DigitalClock/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitalClock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random random = new Random();
+
+        private Color GetRandomColour()
+        {
+            return Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeLbl.Text = DateTime.Now.ToString("HH:mm:ss");
+            dateLbl.Text = DateTime.Now.ToString("yyyy MM dd");
+
+            if((DateTime.Now.Minute) % 2 == 0)
+            {
+                timeLbl.ForeColor = GetRandomColour();
+            }
+            timeLbl.ForeColor = GetRandomColour();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
